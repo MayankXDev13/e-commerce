80a5f420a031519ec161427bcbe064f07b45ae54
Mayank <vijaymaynk2003@gmail.com>
2025-10-16 10:51:39 +0530

    generating migration

--- backend/src/db/db.ts
+++ backend/src/db/db.ts
@@ -1,5 +1,4 @@
 import "dotenv/config";
 import { drizzle } from "drizzle-orm/node-postgres";
-import * as schema from "./schema";
 
-export const db = drizzle(process.env.DATABASE_URL! as string, { schema });
+export const db = drizzle(process.env.DATABASE_URL! as string);
